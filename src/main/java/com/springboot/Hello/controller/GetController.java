ba73374cd3314351e87547cdc62d8139eee17e46
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-27 13:37:45 +0900

    GetController

--- src/main/java/com/springboot/Hello/controller/GetController.java
+++ src/main/java/com/springboot/Hello/controller/GetController.java
@@ -1,10 +1,13 @@
 package com.springboot.Hello.controller;
 
-import org.apache.coyote.Request;
+import com.springboot.Hello.domain.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/v1/get-api") // dispatcherServlet이 매핑해줄 Controller를 등록
-public class HelloController {
+import java.util.Map;
+
+@RestController // dispatcherServlet이 매핑해줄 Controller를 등록
+        @RequestMapping("/api/v1/get-api")
+public class GetController {
 
     // RequestMapping 이란  controller가 할당 된 후 어떤 메서드를 실행할지 연결해주는 어노테이션
     @RequestMapping(name = "/hello", method = RequestMethod.GET)    // hello를 get으로 지정
@@ -30,5 +33,17 @@ public class HelloController {
     @GetMapping(value = "/request1")
     public String getVariable2(@RequestParam String name,@RequestParam String email, @RequestParam String organization) {
         return String.format("%s %s %s", name, email, organization);
+    }
+    @GetMapping(value = "/request2")
+    public String getVariable2(@RequestParam Map<String, String> param) {
+        param.entrySet().forEach((map)->{
+            System.out.printf("key:%s value:%s", map.getKey(), map.getValue());
+        });
+        return "request2가 호출 완료되었습니다";
+    }
+
+    @GetMapping(value = "/request3")
+    public String getRequestParam(MemberDto memberDto){
+        return memberDto.toString();
     }
 }
