21b0bbd7e199680903e64731517755d7df051f8d
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-28 11:45:03 +0900

    GetController에 log.info추가

--- src/main/java/com/springboot/Hello/controller/GetController.java
+++ src/main/java/com/springboot/Hello/controller/GetController.java
@@ -1,27 +1,32 @@
 package com.springboot.Hello.controller;
 
 import com.springboot.Hello.domain.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController // dispatcherServlet이 매핑해줄 Controller를 등록
-        @RequestMapping("/api/v1/get-api")
+@RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     // RequestMapping 이란  controller가 할당 된 후 어떤 메서드를 실행할지 연결해주는 어노테이션
     @RequestMapping(name = "/hello", method = RequestMethod.GET)    // hello를 get으로 지정
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
         return "Suyeon";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable(@PathVariable String varialbe) {
+        log.info("getVariable1로 요청이 들어왔습니다. variable:{}", varialbe);
         return varialbe;
     }
 
